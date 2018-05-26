e4c2bd313c72712e017e1fa76a9b9dc7f7634bc1
wiijima <1110018k@gmail.com>
2018-05-26 00:06:59 +0000

    login-authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
-  
+
+  get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
+
   get 'signup', to: 'users#new'
-  resources :users
-  resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
+
+  resources :tasks, only: [:create, :destroy]
 end
