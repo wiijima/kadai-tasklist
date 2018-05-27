92e72d8c3448dca150370265a5b7e5c71fad4de4
wiijima <1110018k@gmail.com>
2018-05-27 06:06:49 +0000

    login-authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,8 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index]
   
   def index
     @users = User.all.page(params[:page])
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @tasks = @user.tasks.order('created_at DESC').page(params[:page])
-    counts(@user)
   end
 
   def new
